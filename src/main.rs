13ac03be47e6ba510da9ca69fd5505ea28431377
kunikuni03 <49485046+kunikuni03@users.noreply.github.com>
2021-12-13 21:35:22 +0900

    add /sleep handle

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
 use std::fs::File;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -9,7 +11,9 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -19,9 +23,13 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, file_name) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK \r\n\r\n", "index.html")
     } else {
         ("HTTP/1.1 404 Not Found\r\n\r\n", "404.html")
     };
@@ -35,3 +43,4 @@ fn handle_connection(mut stream: TcpStream) {
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
+
